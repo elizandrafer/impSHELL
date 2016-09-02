a9986fa8fe90a37d7d127c0a912371109a326110
adielwesley <adiell.bezerra@gmail.com>
2016-09-02 09:34:30 -0400

    Add existing file

--- shell.c
+++ shell.c
@@ -8,6 +8,7 @@
 #define flag 1
 
 char *redIO[6] = {">", "<", ">>", "<<", "|", "tee"};
+int n = 0;
 
 void command_LS(char arg[]) {
 	char dir[100];
@@ -54,39 +55,35 @@ void command_LS(char arg[]) {
 	}
 
 }
-void command_RNM(char comando[]) {
+void command_RNM(char args[]) {
 	char *str;
-	char comando_copia2[100];
+	char args_copy[100];
 	int cont = 0;
 	char origem[100];
 	char destino[100];
-	//printf("%s\n", comando);
-	strcpy(comando_copia2, comando);
-	str = strtok(comando, " ");
 
-
+	strcpy(args_copy, args);
+	
+	str = strtok(args, " ");
 	while(str != NULL){
-		str = strtok(NULL, " ");
+		str = strtok(NULL, " \n");
 		cont++;
 	}
 
-	if(cont != 3){
-		printf("Diferente de 3\n");
+	if(cont != 2){
+		printf("use: rnm <nome_atual_do_arquivo> <novo_nome_do_arquivo>\n");
 		return;
 	}
 
 	cont = 0;
-	str = strtok(comando_copia2, " ");
-	//str = strtok(NULL, " ");
-
+	str = strtok(args_copy, " \n");
 	while(str != NULL){
-		
-		if (cont == 1) {
+		if (cont == 0) {
 			strcpy(origem, str);
-		} else if (cont == 2){
+		} else if (cont == 1){
 			strcpy(destino, str);
 		}
-		str = strtok(NULL, " ");
+		str = strtok(NULL, " \n");
 		cont++;
 	}
 
@@ -98,98 +95,91 @@ void command_RNM(char comando[]) {
 	}
 }
 
-void command_RMV(char comando[]){
+void command_RMV(char args[]){
 	char *str;
-	char comando_copia2[100];
+	char args_copy[100];
 	int cont = 0;
 	char origem[100];
-	//printf("%s\n", comando);
-	strcpy(comando_copia2, comando);
-	str = strtok(comando, " ");
-
-
+	
+	strcpy(args_copy, args);
+	
+	str = strtok(args, " ");
 	while(str != NULL){
 		str = strtok(NULL, " ");
 		cont++;
 	}
 
-	if(cont != 2){
-		printf("Diferente de 2\n");
+	if(cont != 1){
+		printf("use: rmv <nome_do_arquivo>\n");
 		return;
 	}
 
 	cont = 0;
-	str = strtok(comando_copia2, " ");
-	//str = strtok(NULL, " ");
-
+	str = strtok(args_copy, " \n");
 	while(str != NULL){
-		
-		if (cont == 1) {
-			strcpy(origem, str);
-		}
-		str = strtok(NULL, " ");
-		cont++;
+		strcpy(origem, str);
+		str = strtok(NULL, " \n");
 	}
 
 	if(!remove(origem)){
-		printf("Arquivo removido.\n");
+		printf("Arquivo %s removido.\n", origem);
 	}
 	else{
 		printf("Erro ao remover arquivo.\n");
 	}
 }
 
-void command_COPY(char comando[]) {
+void command_COPY(char args[]) {
 	FILE *f1, *f2;
 	char *str;
-	char comando_copia2[100];
+	char args_copy[100];
 	int cont = 0;
 	char origem[100];
 	char destino[100];
 	char aux_arquivo[100];
-	//printf("%s.\n", comando);
-	strcpy(comando_copia2, comando);
-	str = strtok(comando, " ");
-
-
+	
+	//printf("%s.\n", args);
+	strcpy(args_copy, args);
+	
+	str = strtok(args, " \n");
 	while(str != NULL){
 		str = strtok(NULL, " ");
 		cont++;
 	}
 
-	if(cont != 3){
-		printf("Diferente de 3\n");
+	if(cont != 2){
+		printf("Use: cp <nome_do_arquivo> <nome_da_copia>\n");
 		return;
 	}
 
 	cont = 0;
-	str = strtok(comando_copia2, " ");
+	str = strtok(args_copy, " \n");
 	//str = strtok(NULL, " ");
 
 	while(str != NULL){
 		
-		if (cont == 1) {
+		if (cont == 0) {
 			strcpy(origem, str);
-		} else if (cont == 2){
+		} else if (cont == 1){
 			strcpy(destino, str);
 		}
-		str = strtok(NULL, " ");
+		str = strtok(NULL, " \n");
 		cont++;
 	}
 
 	if(!strcmp(origem,destino)) {
-		printf("Erro ao copiar arquivo\n");
+		printf("Erro ao copiar arquivo. Arquivo original e copia tem o mesmo nome.\n");
 		return;
 	}
 
-	//printf("%s\n", origem);
-	//printf("%s\n", destino);
+	//printf("Arquivo de origem: %s\n", origem);
+	//printf("Arquivo de destino: %s\n", destino);
 
 	f1 = fopen(origem, "r");
 	f2 = fopen(destino, "w");
 
 	if (f1==NULL || f2==NULL) {
-		printf("Erro ao copiar arquivo\n");
+		printf("Erro ao copiar arquivo %s\n", origem);
 		return;
 	}
 
@@ -207,65 +197,113 @@ void command_EXIT() {
 	exit(0);
 }
 
-void exec_command(char comando[]) {
+void exec_command(char command[]) {
 	char *str;
-	char comando_copia[100];
-	strcpy(comando_copia,comando);
-	str = strtok(comando_copia, " ");
+	char command_copy[100];
+	strcpy(command_copy,command);
+	str = strtok(command_copy, " ");
 	printf("%s\n", str);
 	if(str != NULL){
 		if (!strcmp(str,"ls")) { // strcmp retorna 0 se as strings são iguais e 1 caso contrário, por isso a lógica é inversa
 			command_LS("./");
 		}
 		else if (!strcmp(str,"rnm")) {
-			command_RNM(comando);
+			command_RNM(command);
 		}
 		else if (!strcmp(str,"cp")) {
-			command_COPY(comando);
+			command_COPY(command);
 		}else if (!strcmp(str,"rmv")){
-			command_RMV(comando);
+			command_RMV(command);
 		} else{
 			printf("Comando não existe\n");
 		}
 	}
 }
 
-void readCommand(char linhaComando[]){
+void readCommand(char commandLine[]){
 
-	char *str, *argumento=NULL;
-	argumento = strrchr(linhaComando, ' ');							//pega a partir 
-	if(argumento!=NULL) argumento = strrchr(linhaComando, ' ')+1;	//pega depois
-	str = strtok(linhaComando, " \n");								//pega antes
+	char *str, *args=NULL;
+
+	args = strchr(commandLine, ' ');			//pega a partir 
+	if(args!=NULL) {
+		args +=1;	//pega depois
+	}
+	str = strtok(commandLine, " \n");						//pega antes
 	//tratar dentro das funcoes argumento==NULL
 
 	if(str != NULL){
-
 		if(!strcmp(str, "ls")){
-			command_LS(argumento);
+			command_LS(args);
 		}else if(!strcmp(str, "rnm")){
-			command_RNM(argumento);
+			command_RNM(args);
 		}else if(!strcmp(str, "cp")){
-			command_COPY(argumento);
+			command_COPY(args);
 		}else if(!strcmp(str, "rmv")){
-			command_RMV(argumento);
+			command_RMV(args);
 		}else if(!strcmp(str, "exit")){
 			command_EXIT();
 		}else{
 			printf("Comando não existe\n");
 		}
 	}
+}
 
+int call_command(char *command, int input; int first, int last) {
+	readCommand(command);
+}
 
+/*
+	Uteis (Sobre pipe e dup2):
+
+	Enviado pela Ana:
+		http://stackoverflow.com/questions/15673333/what-is-the-proper-way-to-pipe-when-making-a-shell-in-c
+	Enviado pela Eli:
+		http://www.cs.loyola.edu/~jglenn/702/S2005/Examples/dup2.html
+	Exemplo comentado de Pipe em C (em portugues):
+		http://www.programacaoprogressiva.net/2014/09/Pipes-em-C-Comunicao-entre-Processos-IPC-Interprocess-Communication.html
+	Sobre dup e dup2 (em portugues):
+		https://www.vivaolinux.com.br/dica/Utilizando-as-chamadas-de-sistema-dup()-e-dup2()
+	Melhor exemplo de pipe para shell:
+		https://gist.github.com/parse/966049
+
+	// Continuar do ´ultimo link
+
+*/
+
+void cleanup(int n) {
+	int i;
+	for(i=0;i<n;i++) {
+		wait(NULL);
+	}
 }
 
 int main() {
 
-	char linhaComando[100], *str;
+	char linhaComando[1024], *command, *next=NULL;
+	int first, last;
 
 	while(flag){
 		printf("> ");
-		fgets(linhaComando, 100, stdin);
-		readCommand(linhaComando);
+		fgets(linhaComando, 1024, stdin);
+
+		first = 1;
+		last = 0;
+		input = 0;
+
+		command = linhaComando;
+		next = strchr(command, '|');
+
+		while (next != NULL) {
+			next = '\0';
+			input = call_command(command, input, fist, last);
+			command = next+1;
+			next = strchr(command, '|');
+			first = 0;
+		}
+		last = 1;
+		input = call_command(command,input,first,last);
+		cleanup(n);
+		n = 0;		
 	}
 
 	return 0;
